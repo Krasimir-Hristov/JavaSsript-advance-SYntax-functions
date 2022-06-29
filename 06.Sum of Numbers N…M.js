9cd9e67eba0312fd8e2934f7ac5f8a308b3d68a3
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-06-29 20:46:37 +0200

    Add files via upload

---
+++ 06.Sum of Numbers N…M.js
@@ -0,0 +1,12 @@
+function solve(n1, n2) {
+let num1 = Number(n1);
+let num2 = Number(n2);
+let result = 0;
+
+for(let i = num1; i <= num2; i ++) {
+    result += i
+}
+return result
+
+}
+solve('-8', '20')
